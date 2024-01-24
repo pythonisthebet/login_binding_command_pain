799ace8247dff6ac2e91a54bd00296f6a993b93f
noam <noamhaner@gmail.com>
2024-01-24 08:54:24 +0200

    added stuff finished servises, user just need the brain and to make the ve wwork

--- login_binding_command_pain/Services/Login_Services.cs
+++ login_binding_command_pain/Services/Login_Services.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using login_binding_command_pain.models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,40 @@ namespace login_binding_command_pain.Services
 {
     public class Login_Services
     {
+        private List<User> userlist;
+
+        public List<User> Userlist
+        {
+            get { return userlist; }
+            set { userlist = value; }
+        }
+        public Login_Services() 
+        {
+            userlist = new List<User>
+            {
+                new User("adam", "7h7hh"),
+                new User("shahar", "69420"),
+                new User("yarden","goyardigo"),
+                new User("ofek","987"),
+                new User("god","bible"),
+                new User("noam","jk"),
+                new User("mom","mom"),
+                new User("animewaifu69","love"),
+                new User("21pailots","death"),
+                new User("larry","##stillalive")
+            };
+        }
+
+        public bool inlist(User user)
+        {
+            if (Userlist.Contains(user)) return true;
+            else return false;
+        }
+
+        
+
+
     }
+
+
 }
--- login_binding_command_pain/view_models/View_Model.cs
+++ login_binding_command_pain/view_models/View_Model.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using login_binding_command_pain.models;
+using login_binding_command_pain.Services;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -9,6 +11,27 @@ namespace login_binding_command_pain.view_models
 {
     public class View_Model : INotifyPropertyChanged
     {
+        private Border border;
+
+        private string lable;
+
+        public string Lable
+        {
+            get { return lable; }
+            set { lable = value; }
+        }
+
+        public void setColor(Color c)
+        {
+            border.BackgroundColor = c;
+        }
+
+        public void setvisbility(bool visibility)
+        {
+            border.IsVisible = visibility;
+        }
+
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
@@ -20,13 +43,19 @@ namespace login_binding_command_pain.view_models
 
         public View_Model()
         {
-            clickCommand = new Command<string>((p) => checkiftrue(p));
-            
+            clickCommand = new Command<string[] >((p) => checkiftrue(p));
+            border = new Border();
         }
 
-        private bool checkiftrue(string p)
+        private bool checkiftrue(string[] p)
         {
-            return true;
+            User user = new User(p[0], p[1]);
+            if (Login_Services.inlist(user))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
